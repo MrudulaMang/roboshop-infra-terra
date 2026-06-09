a888ec4459493ee84bc655d92e904e5fff37deea
mrudu <mrudu@example.com>
2026-06-09 17:58:44 +0530

    add/modify

--- 70-acm/main.tf
+++ 70-acm/main.tf
@@ -1,5 +1,7 @@
 resource "aws_acm_certificate" "roboshop" {
-  domain_name       = "*.${var.domain_name}"
+  #domain_name       = "*.${var.domain_name}"
+  domain_name               = var.domain_name
+  subject_alternative_names = ["*.${var.domain_name}"]
   validation_method = "DNS"
 
   tags = merge(
